8047712b4b5dd1360d520c7845c351ec87486de5
Eddy Drew <eddy@xcap.ca>
2018-05-31 17:58:26 -0400

    Calculator working

--- src/MortgageCalculator.js
+++ src/MortgageCalculator.js
@@ -5,19 +5,48 @@ class MortgageCalculator extends Component {
 
   state = {
     principleValue: 0,
+    downPayment: 0,
     interestRateValue: 0,
     amortizationPeriod: 0,
-    monthlyPayments: 0
+    payment: 0
   }
 
-  //calculation logic
+  mortgageCalculation() {
 
-  //event handlers
+    let rate = this.refs.interestInput.value;
+    let term = this.refs.amortizationInput.value;
+    let principal = this.refs.principalInput.value;
+
+      //can use a whole number percentage or decimal
+      if(rate > 1) {
+        rate = rate * 0.01;
+      } else {
+        rate = rate;
+      }
+      //can accept the term in years
+      if(term <= 30) {
+        term = term * 12;
+      } else {
+        term = term;
+      }
+
+    let monthlyRate = rate/12;
+    let factor = Math.pow(monthlyRate + 1, term);
+    let numerator = monthlyRate * factor;
+    let denominator = factor - 1;
+    let quotient = numerator/denominator;
+    let payment = principal * quotient;
+    return payment.toFixed(2);
+  }
+
   onInput = (e) => {
     this.setState({
-      principleValue: this.refs.principleInput.value,
-      interestRateValue: this.refs.principleInput.value,
-      amortizationPeriod: this.refs.amortizationInput.value
+      principleValue: this.refs.principalInput.value,
+      downPayment: this.refs.downPaymentInput.value,
+      interestRateValue: this.refs.interestInput.value,
+      amortizationPeriod: this.refs.amortizationInput.value,
+      mortgageAmount: this.principalValue - this.downPayment,
+      payment: this.mortgageCalculation()
     })
   }
 
@@ -28,18 +57,27 @@ class MortgageCalculator extends Component {
       <div>
         <h1>React Mortgage Calculator</h1>
         <form className="Form">
-          <h2>Principle</h2>
-          <input type="text" value={this.state.principleValue} ref="principleInput" onChange={this.onInput.bind(this)} />
+
+          <h2>Mortgage Value</h2>
+          <input type="number" value={this.state.principalValue} ref="principalInput" onChange={this.onInput.bind(this)} />
+
+          <h2>Down Payment</h2>
+          <input type="number" value={this.state.downPayment} ref="downPaymentInput" onChange={this.onInput.bind(this)} />
+
           <h2>Interest</h2>
-          <input type="text" value={this.state.interestRateValue} ref="interestInput" onChange={this.onInput.bind(this)} />
-          <h2>Mortage Duration</h2>
-          <input type="text" value={this.state.monthlyPayments} ref="amortizationInput" onChange={this.onInput.bind(this)} />
+          <input type="number" value={this.state.interestRateValue} ref="interestInput" onChange={this.onInput.bind(this)} />
+
+          <h2>Amortization (Years)</h2>
+          <input type="number" value={this.state.amortizationPeriod} ref="amortizationInput" onChange={this.onInput.bind(this)} />
 
         </form>
 
         <br />
-
-        <h2>Your monthly mortgage payments are: {this.state.monthlyPayments}</h2>
+        <h2>Your property value is: $ {this.state.principleValue}</h2>
+        <h2>Your down payment is: $ {this.state.downPayment}</h2>
+        <h2>Your interest rate is: {this.state.interestRateValue}%</h2>
+        <h2>Your interest amortization period is: {this.state.amortizationPeriod} years</h2>
+        <h2>Your monthly mortgage payments are: {this.state.payment}</h2>
       </div>
     )
   }
